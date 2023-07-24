8a1e4d5b391771873dd04f7cce697a9e67f20b7a
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-07-24 13:57:20 +0800

    set route and implement App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Weather from './Weather';
 
-function App() {
+const App = () => {
+  const divStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Adjust the height based on your requirement
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle}>
+      <div>
+        <h1>My Weather App</h1>
+        <Weather />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
